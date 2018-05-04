00c47e94b30fcaeab6559bc7db9a39b8d9da863e
“yangyunfeng666 <445482919@qq.com>
2018-05-04 19:55:22 +0800

    自定义circleprogress未完

--- app/src/main/java/com/yunsoft/mvpdemo/activity/MyViewActivity.java
+++ app/src/main/java/com/yunsoft/mvpdemo/activity/MyViewActivity.java
@@ -1,7 +1,9 @@
 package com.yunsoft.mvpdemo.activity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.kye.basemodule.glide.GlideUtils;
 import com.yunsoft.mvpdemo.R;
 import com.yunsoft.mvpdemo.mvp.BaseMvpActivity;
 
@@ -13,6 +15,9 @@ import com.yunsoft.mvpdemo.mvp.BaseMvpActivity;
 
 
 public class MyViewActivity extends BaseMvpActivity {
+    private ImageView image;
+    public static final String girl = "https://raw.githubusercontent.com/sfsheng0322/GlideImageView/master/screenshot/girl.jpg";
+
     @Override
     protected void onCreateBefore() {
 
@@ -21,6 +26,8 @@ public class MyViewActivity extends BaseMvpActivity {
     @Override
     protected void initViews(Bundle savedInstanceState) {
         setContentView(R.layout.activity_my_view);
+        image = findViewById(R.id.image);
+        GlideUtils.loadCircleWithBorderImageView(girl,image,1,getResources().getColor(R.color.blue));
     }
 
     @Override
--- basemodule/src/main/java/com/kye/basemodule/glide/GlideUtils.java
+++ basemodule/src/main/java/com/kye/basemodule/glide/GlideUtils.java
@@ -1,9 +1,16 @@
 package com.kye.basemodule.glide;
 
 import android.net.Uri;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.Dimension;
+import android.support.annotation.DrawableRes;
 import android.widget.ImageView;
 
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
+import com.kye.basemodule.glide.transformation.GlideCircleTransformation;
+import com.kye.basemodule.glide.transformation.GlideCircleWithBorderTransformation;
 
 /**
  * Created by yyf on 2018-04-17 10:17.
@@ -49,6 +56,37 @@ public class GlideUtils {
         GlideApp.with(imageView.getContext()).load(url).apply(requestOptions).into(imageView);
     }
 
+    /**
+     * 加载圆形图像
+     * @param url
+     * @param imageView
+     */
+    public static void loadCircleImageView(String url,ImageView imageView){
+        GlideApp.with(imageView.getContext()).asBitmap().load(url).centerCrop().transform(new GlideCircleTransformation())
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(imageView);
+    }
 
+    /**
+     * 加载圆形图像
+     * @param url
+     * @param imageView
+     * @param placeHolder
+     */
+    public static void loadCircleImageView(String url,ImageView imageView,@DrawableRes int placeHolder){
+        GlideApp.with(imageView.getContext()).asBitmap().centerCrop().placeholder(placeHolder).transform(new GlideCircleTransformation())
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE).into(imageView);
+    }
 
+    /**
+     * 加载圆形图像而且也边框
+     * @param url
+     * @param imageView
+     */
+    public static void loadCircleWithBorderImageView(String url, ImageView imageView,  int borderWith,  int borderColor){
+        GlideApp.with(imageView.getContext()).asBitmap().load(url)
+                .centerCrop()
+                .transform(new GlideCircleWithBorderTransformation(borderWith,borderColor))
+                .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
+                .into(imageView);
+    }
 }
